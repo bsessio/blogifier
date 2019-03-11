da697018779f0768989e796ac7a4b8ec28f522e4
Jonathan169 <43359787+Jonathan169@users.noreply.github.com>
2019-03-11 18:37:47 -0500

    jons manual commit

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ module.exports=function(passport){
     router.post("/signup",function(req,res){
         var username=req.body.username,
             password=req.body.password;
+            area=req.body.area;
         user.findOne({username:username}).then(function(err,data){
             if (err){res.status(500).send("Error: "+err)}
             else{
@@ -17,13 +18,23 @@ module.exports=function(passport){
                         username:username,
                         password:newuser.hashpassword(password)
                     }).then(function(data){
+                        console.log(data)
                         res.json(data)
                     })
                     
                 }
             }
-    
         })
+    });
+    router.post("/login",passport.authenticate('local',{
+        failureRedirect:"/login",
+        successRedirect:"/"
+    }),function(req,res){
+        res.json(res)
+    })
+    router.get('/logout',function(req,res){
+        req.logout()
+        res.send("/")
     })
     return router;
 }
