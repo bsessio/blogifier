b42f12ca246945c6f8f513d868d7f5865bebbbe0
Tasha <shradertasha@gmail.com>
2019-03-12 11:43:01 -0500

    C:/Program Files/Git/app add

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
 DROP DATABASE IF EXISTS blogifier_db;
 -- Creates the "blogger" database --
-CREATE DATABASE blogifie_db;
+CREATE DATABASE blogifier_db;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,19 +5,19 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/register.html"));
+    res.sendFile(path.join(__dirname, "../public/app/register.html"));
   });
 
   app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/app/index.html"));
   });
 
   app.get("/post", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/post.html"));
+    res.sendFile(path.join(__dirname, "../public/app/post.html"));
   });
 
   app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/app/login.html"));
   });
 };
 
